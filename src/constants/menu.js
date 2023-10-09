94150ea41e6e7a27e55004f5e35ffb9e4761aeb9
mimizallah <imizallah@gmail.com>
2023-10-09 13:28:37 +0100

    Side bar done

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -11,13 +11,11 @@ const data = [
       {
         icon: 'simple-icon-home',
         label: 'Dashboard',
-        to: `${adminRoot}/dashboards/content`,
+        to: `${adminRoot}/dashboards/default`,
         // roles: [UserRole.Admin],
       }
     ],
   },
-
-
 
   {
     id: 'users',
@@ -26,151 +24,28 @@ const data = [
     to: `${adminRoot}/users`,
     subs: [
       {
-        id: 'pages-authorization',
-        label: 'menu.authorization',
-        to: '/user',
-        subs: [
-          {
-            icon: 'simple-icon-user-following',
-            label: 'menu.login',
-            to: '/user/login',
-            newWindow: true,
-          },
-          {
-            icon: 'simple-icon-user-follow',
-            label: 'menu.register',
-            to: '/user/register',
-            newWindow: true,
-          },
-          {
-            icon: 'simple-icon-user-following',
-            label: 'menu.forgot-password',
-            to: '/user/forgot-password',
-            newWindow: true,
-          },
-          {
-            icon: 'simple-icon-user-unfollow',
-            label: 'menu.reset-password',
-            to: '/user/reset-password',
-            newWindow: true,
-          },
-        ],
+        icon: 'simple-icon-people',
+        label: 'All Users',
+        to: `${adminRoot}/users/all-users`,
       },
       {
-        id: 'transactions',
-        label: 'menu.product',
-        to: `${adminRoot}/transactions`,
-        subs: [
-          {
-            icon: 'simple-icon-credit-card',
-            label: 'menu.data-list',
-            to: `${adminRoot}/pages/product/data-list`,
-          },
-          {
-            icon: 'simple-icon-list',
-            label: 'menu.thumb-list',
-            to: `${adminRoot}/pages/product/thumb-list`,
-          },
-          {
-            icon: 'simple-icon-grid',
-            label: 'menu.image-list',
-            to: `${adminRoot}/pages/product/image-list`,
-          },
-          {
-            icon: 'simple-icon-picture',
-            label: 'menu.details',
-            to: `${adminRoot}/pages/product/details`,
-          },
-          {
-            icon: 'simple-icon-book-open',
-            label: 'menu.details-alt',
-            to: `${adminRoot}/pages/product/details-alt`,
-          },
-        ],
+        icon: 'simple-icon-user-follow',
+        label: 'Create New Account',
+        to: `${adminRoot}/users/new-user`,
       },
       {
-        id: 'credit-card',
-        label: 'menu.profile',
-        to: `${adminRoot}/credit-card`,
-        subs: [
-          {
-            icon: 'simple-icon-share',
-            label: 'menu.social',
-            to: `${adminRoot}/credit-card/social`,
-          },
-          {
-            icon: 'simple-icon-link',
-            label: 'menu.portfolio',
-            to: `${adminRoot}/credit-card/portfolio`,
-          },
-        ],
+        icon: 'iconsminds-credit-card',
+        label: 'Credit/Debit User',
+        to: `${adminRoot}/users/credit-debit-user`,
       },
       {
-        id: 'pages-blog',
-        label: 'menu.blog',
-        to: `${adminRoot}/pages/blog`,
-        subs: [
-          {
-            icon: 'simple-icon-share',
-            label: 'menu.blog-list',
-            to: `${adminRoot}/pages/blog/blog-list`,
-          },
-          {
-            icon: 'simple-icon-link',
-            label: 'menu.blog-detail',
-            to: `${adminRoot}/pages/blog/blog-detail`,
-          },
-        ],
-      },
-
-
-      {
-        id: 'pages-miscellaneous',
-        label: 'menu.miscellaneous',
-        to: `${adminRoot}/pages/miscellaneous`,
-        subs: [
-          {
-            icon: 'simple-icon-question',
-            label: 'menu.faq',
-            to: `${adminRoot}/pages/miscellaneous/faq`,
-          },
-          {
-            icon: 'simple-icon-graduation',
-            label: 'menu.knowledge-base',
-            to: `${adminRoot}/pages/miscellaneous/knowledge-base`,
-          },
-
-          {
-            icon: 'simple-icon-diamond',
-            label: 'menu.prices',
-            to: `${adminRoot}/pages/miscellaneous/prices`,
-          },
-          {
-            icon: 'simple-icon-magnifier',
-            label: 'menu.search',
-            to: `${adminRoot}/pages/miscellaneous/search`,
-          },
-          {
-            icon: 'simple-icon-envelope-open',
-            label: 'menu.mailing',
-            to: `${adminRoot}/pages/miscellaneous/mailing`,
-          },
-          {
-            icon: 'simple-icon-bag',
-            label: 'menu.invoice',
-            to: `${adminRoot}/pages/miscellaneous/invoice`,
-          },
-
-          {
-            icon: 'simple-icon-exclamation',
-            label: 'menu.error',
-            to: '/error',
-            newWindow: true,
-          },
-        ],
+        icon: 'iconsminds-mail-money',
+        label: 'Wire Transfer User',
+        to: `${adminRoot}/users/wire-transfer`,
       },
     ],
   },
+
   {
     id: 'transactions',
     icon: 'iconsminds-credit-card',
@@ -178,19 +53,29 @@ const data = [
     to: `${adminRoot}/transactions`,
     subs: [
       {
-        icon: 'simple-icon-check',
-        label: 'menu.todo',
-        to: `${adminRoot}/applications/todo`,
+        icon: 'iconsminds-credit-card',
+        label: 'Credit/Debit Transaction',
+        to: `${adminRoot}/transactions/credit-debit`,
       },
       {
-        icon: 'simple-icon-calculator',
-        label: 'menu.survey',
-        to: `${adminRoot}/applications/survey`,
+        icon: 'iconsminds-credit-card',
+        label: 'Witdraw Transaction',
+        to: `${adminRoot}/transactions/survey`,
       },
       {
-        icon: 'simple-icon-bubbles',
-        label: 'menu.chat',
-        to: `${adminRoot}/applications/chat`,
+        icon: 'iconsminds-credit-card',
+        label: 'Crypto Transaction',
+        to: `${adminRoot}/transactions/crypto-transactions`,
+      },
+      {
+        icon: 'iconsminds-credit-card',
+        label: 'Domestic Transaction',
+        to: `${adminRoot}/transactions/domestic-transactions`,
+      },
+      {
+        icon: 'iconsminds-credit-card',
+        label: 'Loan Request',
+        to: `${adminRoot}/transactions/loan-requests`,
       },
     ],
   },
@@ -201,130 +86,11 @@ const data = [
     to: `${adminRoot}/credit-card`,
     subs: [
       {
-        id: 'ui-forms',
-        label: 'menu.forms',
-        to: `${adminRoot}/ui/forms`,
-        subs: [
-          {
-            icon: 'simple-icon-notebook',
-            label: 'menu.layouts',
-            to: `${adminRoot}/ui/forms/layouts`,
-          },
-          {
-            icon: 'simple-icon-puzzle',
-            label: 'menu.components',
-            to: `${adminRoot}/ui/forms/components`,
-          },
-          {
-            icon: 'simple-icon-check',
-            label: 'menu.validations',
-            to: `${adminRoot}/ui/forms/validations`,
-          },
-          {
-            icon: 'simple-icon-magic-wand',
-            label: 'menu.wizard',
-            to: `${adminRoot}/ui/forms/wizard`,
-          },
-        ],
-      },
-      {
-        id: 'ui-components',
-        label: 'menu.components',
-        to: `${adminRoot}/ui/components`,
-        subs: [
-          {
-            icon: 'simple-icon-bell',
-            label: 'menu.alerts',
-            to: `${adminRoot}/ui/components/alerts`,
-          },
-          {
-            icon: 'simple-icon-badge',
-            label: 'menu.badges',
-            to: `${adminRoot}/ui/components/badges`,
-          },
-          {
-            icon: 'simple-icon-control-play',
-            label: 'menu.buttons',
-            to: `${adminRoot}/ui/components/buttons`,
-          },
-          {
-            icon: 'simple-icon-layers',
-            label: 'menu.cards',
-            to: `${adminRoot}/ui/components/cards`,
-          },
-          {
-            icon: 'simple-icon-picture',
-            label: 'menu.carousel',
-            to: `${adminRoot}/ui/components/carousel`,
-          },
-          {
-            icon: 'simple-icon-chart',
-            label: 'menu.charts',
-            to: `${adminRoot}/ui/components/charts`,
-          },
-          {
-            icon: 'simple-icon-arrow-up',
-            label: 'menu.collapse',
-            to: `${adminRoot}/ui/components/collapse`,
-          },
-          {
-            icon: 'simple-icon-arrow-down',
-            label: 'menu.dropdowns',
-            to: `${adminRoot}/ui/components/dropdowns`,
-          },
-          {
-            icon: 'simple-icon-book-open',
-            label: 'menu.editors',
-            to: `${adminRoot}/ui/components/editors`,
-          },
-
-          {
-            icon: 'simple-icon-star',
-            label: 'menu.icons',
-            to: `${adminRoot}/ui/components/icons`,
-          },
-          {
-            icon: 'simple-icon-note',
-            label: 'menu.input-groups',
-            to: `${adminRoot}/ui/components/input-groups`,
-          },
-          {
-            icon: 'simple-icon-screen-desktop',
-            label: 'menu.jumbotron',
-            to: `${adminRoot}/ui/components/jumbotron`,
-          },
-          {
-            icon: 'simple-icon-map',
-            label: 'menu.maps',
-            to: `${adminRoot}/ui/components/maps`,
-          },
-          {
-            icon: 'simple-icon-docs',
-            label: 'menu.modal',
-            to: `${adminRoot}/ui/components/modal`,
-          },
-          {
-            icon: 'simple-icon-cursor',
-            label: 'menu.navigation',
-            to: `${adminRoot}/ui/components/navigation`,
-          },
-          {
-            icon: 'simple-icon-pin',
-            label: 'menu.popover-tooltip',
-            to: `${adminRoot}/ui/components/popover-tooltip`,
-          },
-          {
-            icon: 'simple-icon-shuffle',
-            label: 'menu.sortable',
-            to: `${adminRoot}/ui/components/sortable`,
-          },
-          {
-            icon: 'simple-icon-grid',
-            label: 'menu.tables',
-            to: `${adminRoot}/ui/components/tables`,
-          },
-        ],
-      },
+        icon: 'iconsminds-credit-card',
+        label: 'Virtual Cards',
+        to: `${adminRoot}/cards/virtual-cards`,
+      }
+      
     ],
   },
   
@@ -335,32 +101,28 @@ const data = [
     to: `${adminRoot}/settings`,
     subs: [
       {
-        icon: 'simple-icon-logout',
-        label: 'menu.types',
-        to: `${adminRoot}/menu/types`,
+        icon: 'simple-icon-user',
+        label: 'Profile',
+        to: `${adminRoot}/settings/profile`,
       },
       {
-        icon: 'simple-icon-layers',
-        label: 'menu.levels',
-        to: `${adminRoot}/menu/levels`,
-        subs: [
-          {
-            icon: 'simple-icon-arrow-right',
-            label: 'menu.third-level-1',
-            to: `${adminRoot}/menu/levels/third-level-1`,
-          },
-          {
-            icon: 'simple-icon-arrow-right',
-            label: 'menu.third-level-2',
-            to: `${adminRoot}/menu/levels/third-level-2`,
-          },
-          {
-            icon: 'simple-icon-arrow-right',
-            label: 'menu.third-level-3',
-            to: `${adminRoot}/menu/levels/third-level-3`,
-          },
-        ],
+        icon: 'iconsminds-credit-card',
+        label: 'Deposit Method',
+        to: `${adminRoot}/settings/deposit-method`,
+        
       },
+      {
+        icon: 'iconsminds-mail-money',
+        label: 'Virtual Deposit',
+        to: `${adminRoot}/settings/virtual-deposit`,
+        
+      },
+      {
+        icon: 'simple-icon-settings',
+        label: 'Settins',
+        to: `${adminRoot}/settings/setting`,
+        
+      }
     ],
   }
 ];
